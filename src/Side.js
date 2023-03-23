bfe0c464f893f9525f1129035e50ba43319eefce
yeonsukkk <dustn910720@gmail.com>
2023-03-23 22:30:22 +0900

    레이아웃 수정

--- src/Side.js
+++ src/Side.js
@@ -5,10 +5,10 @@ const Side = () => {
         <div id="desktop">
             <h2><i>T</i>ravel</h2>
             <p><img src={bgTravel} alt="여행이미지" /></p>
-            <div className="qr">
+            {/* <div className="qr">
                 <img src="" alt="QR코드" />
                 <p>모바일에서 확인하세요~!</p>
-            </div>
+            </div> */}
         </div>
     )
 }
